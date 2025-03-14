53f679da28dc3878a1af0730b0acc533c1c0dda2
fgeiger <florian.geiger21@gmail.com>
2025-03-14 16:30:02 +0100

    created a detail view layout

---
+++ Fruits/App/FruitDetailView.swift
@@ -0,0 +1,52 @@
+//
+//  FruitDetailView.swift
+//  Fruits
+//
+//  Created by Florian Geiger on 14.03.25.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    var fruit : Fruit
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical,showsIndicators: false){
+                VStack(alignment: .center, spacing: 20){
+                    FruitHeaderView(fruit: fruit)
+                    
+                    VStack (alignment: .leading, spacing: 20){
+                        
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        Text("Learn mor about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                            
+                    }
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }
+            }
+            .edgesIgnoringSafeArea(.top)
+        }
+    }
+}
+
+#Preview {
+    FruitDetailView(fruit: fruitsData[0])
+}
