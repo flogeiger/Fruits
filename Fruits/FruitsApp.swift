0e87d44a7a66cf4528e213108ccf6b0af79144f1
fgeiger <florian.geiger21@gmail.com>
2025-03-14 14:08:24 +0100

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Florian Geiger on 14.03.25.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FruitsTests/FruitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsTests.swift
+//  FruitsTests
+//
+//  Created by Florian Geiger on 14.03.25.
+//
+
+import Testing
+@testable import Fruits
+
+struct FruitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
