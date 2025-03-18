3a6831caccbebe7d3f17e507ca00fdcd8aa73a69
fgeiger <florian.geiger21@gmail.com>
2025-03-18 20:32:00 +0100

    created SettingsView

--- Fruits/App/ContentView.swift
+++ Fruits/App/ContentView.swift
@@ -8,20 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    var fruits: [Fruit] = fruitsData
-    var body: some View {
-        NavigationView{
-            List{
-                ForEach(fruits.shuffled()){ item in
-                    NavigationLink(destination: FruitDetailView(fruit: item)){
-                        FruitRowView(fruit: item)
-                            .padding(.vertical,4)
-                    }
-                }
-            }
-            .navigationTitle("Fruits")
+  // MARK: - PROPERTIES
+  
+  @State private var isShowingSettings: Bool = false
+  
+  var fruits: [Fruit] = fruitsData
+
+  // MARK: - BODY
+
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(fruits.shuffled()) { item in
+          NavigationLink(destination: FruitDetailView(fruit: item)) {
+            FruitRowView(fruit: item)
+              .padding(.vertical, 4)
+          }
         }
-    }
+      }
+      .navigationTitle("Fruits")
+      .navigationBarItems(
+        trailing:
+          Button(action: {
+            isShowingSettings = true
+          }) {
+            Image(systemName: "slider.horizontal.3")
+          } //: BUTTON
+          .sheet(isPresented: $isShowingSettings) {
+            SettingsView()
+          }
+      )
+    } //: NAVIGATION
+    .navigationViewStyle(StackNavigationViewStyle())
+  }
 }
 
 #Preview {
